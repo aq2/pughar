ded0d3bb9e8c873f37bd677719c1c8314bbae0bb
angelo <angelo@fluxton.co.uk>
2019-06-06 15:58:55 +0100

    code and images moved

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,26 @@ var stylus = require('gulp-stylus')
 var rename = require('gulp-rename')
 var sourcemaps = require('gulp-sourcemaps')
 
-var pugDist = './dist/'
-var styleDest = './dist/css/'
-var pugSrc = './src/index.pug'
-var styleSrc = './src/stylus/main.styl'
+
+var pugSrc = 'src/index.pug'
+var pugWatch = 'src/pages/**/*.pug'
+var pugDest = './dist/'
+
+var stylusSrc = 'src/stylus/main.styl'
+var stylusWatch = 'src/stylus/**/*.styl'
+var stylusDest = './dist/css/'
+
+var codeWatch = 'src/code/**/*.*'
+var codeDest = './dist/code'
+
+var imagesWatch = './src/images/**/*.*'
+var imagesDest = './dist/images'
+
+
 
 
 gulp.task('stylus', done => {
-  gulp.src( styleSrc )
+  gulp.src( stylusSrc )
       .pipe( sourcemaps.init())
       .pipe( stylus({
         compress: true,
@@ -20,15 +32,40 @@ gulp.task('stylus', done => {
       .on( 'error', console.error.bind(console))
       .pipe( rename( { suffix: '.min' }))
       .pipe( sourcemaps.write( './') )
-      .pipe( gulp.dest( styleDest ))
+      .pipe( gulp.dest( stylusDest ))
   done()
-});
+})
 
 
 gulp.task('pug', done => {
   gulp.src( pugSrc )
       .pipe( pug())
-      .pipe( gulp.dest( pugDist ))
+      .pipe( gulp.dest( pugDest ))
   done()
-});
+})
 
+
+gulp.task('code', done => {
+  gulp.src( codeWatch)
+      .pipe( gulp.dest( codeDest))
+  done()
+})
+
+
+gulp.task('images', done => {
+  gulp.src( imagesWatch)
+      .pipe( gulp.dest( imagesDest ))
+  done()
+})
+
+
+
+gulp.task('default', gulp.series([ 'stylus', 'pug', 'code', 'images']))
+
+gulp.task('watch', gulp.parallel( 'default', done => {
+  gulp.watch( stylusWatch, gulp.series( 'stylus'))
+  gulp.watch( codeWatch, gulp.series( 'code'))
+  gulp.watch( pugWatch, gulp.series( 'pug'))
+  gulp.watch( imagesSrc, gulp.series( 'images'))
+  done()
+}))
--- src/code/tabs.js
+++ src/code/tabs.js
@@ -1,3 +1,23 @@
+
+window.open = function () {};
+window.print = function () {};
+
+// Support hover state for mobile.
+if (false) { window.ontouchstart = function () {}; }
+
+function __linkClick(e) {	parent.window.postMessage(this.href, '*'); };
+
+function __bindToLinks() {
+  var __links = document.querySelectorAll('a');
+  for (var i = 0, l = __links.length; i < l; i++) {
+    if (__links[i].getAttribute('data-t') == '_blank') {
+      __links[i].addEventListener('click', __linkClick, false);
+    }
+  }
+}
+
+
+
 if (document.location.search.match(/type=embed/gi)) {
   window.parent.postMessage('resize', "*");
 }
