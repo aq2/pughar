5823ffdbf79eff7939a28b6f145ac939fd438ea2
angelo <angelo@fluxton.co.uk>
2019-06-10 19:50:19 +0100

    fink i sorted gulp

--- gulpfile.js
+++ gulpfile.js
@@ -18,34 +18,45 @@ const logError = (err) => {
 
 
 function styles() {
-  return gulp.src('./src/stylus/**/*.styl')
-            .pipe(errorHandler(logError))
-            .pipe(sourcemaps.init())
-            .pipe(stylus({ compress: true }))
-            .pipe(rename({ suffix: '.min' }))
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest('./www/css'))
-            .pipe(browserSync.stream())
+  return gulp.src('./src/stylus/main.styl')
+             .pipe(errorHandler(logError))
+             .pipe(sourcemaps.init())
+             .pipe(stylus({ compress: true }))
+             .pipe(rename({ suffix: '.min' }))
+             .pipe(sourcemaps.write('./'))
+             .pipe(gulp.dest('./www/css'))
+             .pipe(browserSync.stream())
 }
 exports.styles = styles
 
 
-function includes() {
-  return gulp.src('./src/**/index.pug')
-            .pipe(errorHandler(logError))
-            .pipe(pug())
-            .pipe(gulp.dest('./www/'))
+function index() {
+  return gulp.src('./src/index.pug')
+             .pipe(errorHandler(logError))
+             .pipe(pug())
+             .pipe(gulp.dest('./www/'))
 }
-exports.includes = includes
+exports.index = index
 
 
 function pages() {
-  return gulp.src('./src/**/*.pug')
+  return gulp.src('src/pages/**/*.pug')
             .pipe(errorHandler(logError))
             .pipe(pug())
             .pipe(gulp.dest('./www/pages'))
 }
 exports.pages = pages
+
+
+function pugStuff() {
+  return gulp.src([
+              'src/includes/**/*.pug',
+              'src/mixins/*.pug',
+             ])
+            .pipe(errorHandler(logError))
+            .pipe(pug())
+}
+exports.pugStuff = pugStuff
 
 
 function codes() {
@@ -65,29 +76,38 @@ exports.images = images
 
 function watch() {
   browserSync.init({ server: './www' })
+
   gulp.watch('./src/code/**/*.*', codes)
   gulp.watch('./src/images/**/*.*', images)
   gulp.watch('./src/stylus/**/*.styl', styles)
-  gulp.watch('./src/**/*.pug', pages).on('change', browserSync.reload)
-  // gulp.watch('./src/pages/**/*.*', pages).on('change', browserSync.reload)
+  gulp.watch('./src/index.pug', index).on('change', browserSync.reload)
+  gulp.watch('./src/pages/**/*.pug', pages).on('change', browserSync.reload)
+  gulp.watch('./src/mixins/**.pug', pugStuff).on('change', browserSync.reload)
+  gulp.watch('./src/includes/**.pug', pugStuff).on('change', browserSync.reload)
 
-  gulp.src('./src/index.*').pipe(notify('Gulp up and running 😃 '));
+  gulp.src('./src/index.*').pipe(notify('👓 Gulp up, running and watching 👓'))
 }
 exports.watch = watch
 
 
 function nuke() {
-  gulp.src("./src").pipe(notify(' gulp nuke and pave'))
+  gulp.src('./src').pipe(notify('😱 Gulp nuke and pave 😱'))
   return del('./www/**/*')
 }
 exports.nuke = nuke
 
 
-const gza = function (cb) {
-  gulp.src("./www").pipe(notify('gulp building to be born'))
-  cb()
+function gza() {
+  return gulp.src('./www/index.html').pipe(notify('🏠 Gulp building to be born 🏠'))
 }
+exports.gza = gza
 
-
-gulp.task('build', gulp.series(nuke, includes, gulp.parallel(pages, codes, images, styles), gza))
+gulp.task('default',
+            gulp.series(
+              nuke,
+              gulp.parallel(index, pages, codes, images, styles),
+              gza,
+              watch
+            )
+)
 
