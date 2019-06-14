36cf5bd5f0a2941b9aa3494dac15bae5e9090b37
angelo <angelo@fluxton.co.uk>
2019-06-14 12:08:17 +0100

    flippin forms

--- src/php/simpleFormPhp.php
+++ src/php/simpleFormPhp.php
@@ -1,6 +1,7 @@
 <?php
 
-$EmailTo = "mickey.megabyte@gmail.com";
+$EmailTo = 'me@angelo.xyz';
+// $EmailTo = "mickey.megabyte@gmail.com";
 // $EmailTo = "saghar@ayurvedicyogamassage.org.uk";
 
 $Name = Trim(stripslashes($_POST['name']));
@@ -8,29 +9,47 @@ $Email = Trim(stripslashes($_POST['email']));
 $Message = Trim(stripslashes($_POST['message']));
 $Subject = Trim(stripslashes($_POST['subject']));
 
-$HabitsPage = false;
-if (strpos($Subject, 'contact' !== false)) {
-  $HabitsPage = true;
+$HabitsPage = False;
+$Date = 'fish';
+if (strpos($Subject, 'Ten' == 0)) {
+  $HabitsPage = True;
   $Date = Trim(stripslashes($_POST['date']));
 }
 
 
 // prepare email body text
-$Body = "Name: ";
+$Body = 'Name: ';
 $Body .= $Name;
 $Body .= "\n\n";
-$Body .= "Email: ";
+$Body .= 'email address: ';
 $Body .= $Email;
 $Body .= "\n\n";
 
+
+$Body .= 'Subject: ';
+$Body .= $Subject;
+$Body .= "\n\n";
+
+$Body .= 'strpos: ';
+$Body .= strpos($Subject, 'Ten');
+$Body .= "\n\n";
+
+$Body .= "habits page: ";
+$Body .= $HabitsPage;
+$Body .= "\n\n";
+
 if ($HabitsPage) {
-  $Body .= "Talk date: ";
+  $Body .= 'Talk date: ';
   $Body .= $Date;
   $Body .= "\n\n";
-  $Body .= "Challenge: ";
+  $Body .= 'Challenge: ';
 } else {
-  $Body .= 'Message';
+  $Body .= 'Message: ';
 }
+
+  $Body .= 'Talk date: ';
+  $Body .= $Date;
+  $Body .= "\n\n";
 
 $Body .= $Message;
 $Body .= "\n";
@@ -43,7 +62,7 @@ if ($success){
   print "<meta http-equiv='refresh' content='0;URL=../pages/contactthanks.html'>";
 }
 else{
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=../error.html\">";
+  print "<meta http-equiv='refresh' content='0;URL=../error.html'>";
 }
 
 ?>
