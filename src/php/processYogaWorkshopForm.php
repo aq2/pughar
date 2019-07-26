32dae6e8177ddf7bebed5d30544e7de8af8dbd2f
angelo <angelo@fluxton.co.uk>
2019-07-26 13:15:43 +0100

    forms done - only need pay4training

--- src/php/processYogaWorkshopForm.php
+++ src/php/processYogaWorkshopForm.php
@@ -1,62 +1,72 @@
 <?php
 
-// $EmailTo = 'saghar@ayurvedicyogamassage.org.uk';
-$EmailTo = 'angelo@fluxton.co.uk';
+require('mail/mailer.php');
 
-$Name = safify($_POST['name']);
-$Tel = safify($_POST['tel']);
-$Email = safify($_POST['email']);
-$Tickets = safify($_POST['tickets']);
-$Subject = safify($_POST['subject']);
+// where to send emails to, and where they appear to come from
+$emailTo = 'angelo@fluxton.co.uk';
+// $emailTo = 'saghar@ayurvedicyogamassage.org.uk';
+$emailCC = 'mickey.megabyte@gmail.com';
+
+// read and sanitize form responses
+$form_name = safify($_POST['name']);
+$form_tel = safify($_POST['tel']);
+$form_email = safify($_POST['email']);
+$form_tickets = safify($_POST['tickets']);
+$form_subject = safify($_POST['subject']);
 
 
 // prepare email body text
-$Body = '';
-$Body .= 'Name: ';
-$Body .= $Name;
-$Body .= "\n\n";
-$Body .= 'Phone: ';
-$Body .= $Tel;
-$Body .= "\n\n";
-$Body .= 'Email: ';
-$Body .= $Email;
-$Body .= "\n\n";
-$Body .= 'Workshop: ';
-$Body .= $Subject;
-$Body .= "\n\n";
-$Body .= 'Tickets: ';
-$Body .= $Tickets;
-$Body .= "\n\n\n\n";
-$Body .= "Hi Saghar, little message from angelo...\n";
-$Body .= "This email only means that someone has filled in the workshop form.\n";
-$Body .= "It DOES NOT mean that they have paid.\n";
-$Body .= "PayPal will send you another email when they have paid.\n";
+$body = '<h1>Yoga workshop reply</h1>';
+$body .= "<h2>Name: $form_name </h2>";
+$body .= "<h2>Phone: $form_tel </h2>";
+$body .= "<h2>Email: $form_email </h2>";
+$body .= "<h2>Workshop: $form_subject </h2>";
+$body .= "<h2>Tickets: $form_tickets </h2>";
+$body .= "<br><br>";
+$body .= "<p>Hi Saghar, little message from angelo...</p>";
+$body .= "<p>This email only means that someone has filled in the yoga workshop form.</p>";
+$body .= "<p>It DOES NOT mean that they have paid.</p>";
+$body .= "<p>PayPal will send you another email when they have paid.</p>";
 
 
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From: <$Email>");
+try {
+  // Recipients
+  $mail->addAddress($emailTo);     // Add a recipient
+  $mail->setFrom($emailTo, 'from website');
+  $mail->addReplyTo($form_email);
+  $mail->addCC($emailCC);
+
+  // Content
+  $mail->isHTML(true);
+  $mail->Subject = $form_subject;
+  $mail->Body    = $body;
+  $mail->AltBody = $body;
+
+  $mail->send();
+
+  // transer to paypal payment page
+  $blah = "<meta http-equiv='refresh' content='0;url=https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=";
 
 
-// transer to paypal payment page
-$blah = "<meta http-equiv='refresh' content='0;url=https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=";
-
-if ($success) {
-  switch ($Tickets) {
-  case "single_full":
-    print $blah . "MF8MWMBJ9QKKS" . "'>";
-    break;
-  case "single_remainder":
-    print $blah . "P8SDDMEMR3846" . "'>";
-    break;
-  case "deposit":
-    print $blah . "QA9L8PWDRLWPW" . "'>";
-    break;
-  default:
-    echo "something's gone awry!";
+  switch ($form_tickets) {
+    case "single_full":
+      print $blah . "MF8MWMBJ9QKKS" . "'>";
+      break;
+    case "single_remainder":
+      print $blah . "P8SDDMEMR3846" . "'>";
+      break;
+    case "deposit":
+      print $blah . "QA9L8PWDRLWPW" . "'>";
+      break;
+    default:
+      echo "something's gone awry!";
   }
-} else {
-  print "<meta http-equiv='refresh' content='0;url=../pages/formError.html'>";
+
+} catch (Exception $e) {
+  /* echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; */
+  print "<meta http-equiv='refresh' content='0;URL=../pages/formError.html'>";
 }
+
 
 
 // sanitise user input - don't trust them!
