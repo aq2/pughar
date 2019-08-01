e15f9671a08bfff77e88d08d9e6ce5a3bebf978b
angelo <angelo@fluxton.co.uk>
2019-08-01 14:54:55 +0100

    mixing php and js ftw

--- src/php/approve.php
+++ src/php/approve.php
@@ -14,25 +14,46 @@ $workshops = $_GET['workshops'];
 $quals = $_GET['quals'];
 $subject = $_GET['subject'];
 
+
+/* change all this to a php function to showDetails() */
+
+function showDetails() {
+
+  $name = $_GET['name'];
+  $gender = $_GET['gender'];
+  $age = $_GET['age'];
+  $tel = $_GET['tel'];
+  $email = $_GET['email'];
+  $health = $_GET['health'];
+  $meds = $_GET['meds'];
+  $workshops = $_GET['workshops'];
+  $quals = $_GET['quals'];
+  $subject = $_GET['subject'];
+
+  echo "<h1>Intensive Training Application</h1>";
+
+  echo "<p>Name: $name </p>";
+  echo "<p>Gender: $gender </p>";
+  echo "<p>Age: $age </p>";
+  echo "<p>Tel: $tel </p>";
+  echo "<p>email: $email </p>";
+  echo "<p>health: $health </p>";
+  echo "<p>medications: $meds </p>";
+  echo "<p>workshops taken: $workshops </p>";
+  echo "<p>qualifications: $qualifications </p>";
+  echo "<p>subject: $subject </p>";
+
+}
+
+showDetails();
+
 function yep() {
-  global $name;
+  showDetails();
   echo "$name is good";
 }
 
 ?>
 
-<h1>Intensive Training Application</h1>
-
-<p>Name: <?php echo $name; ?></p>
-<p>Gender: <?php echo $gender; ?></p>
-<p>Age: <?php echo $age; ?></p>
-<p>Tel: <?php echo $tel; ?></p>
-<p>email: <?php echo $email; ?></p>
-<p>health: <?php echo $health; ?></p>
-<p>medications: <?php echo $meds; ?></p>
-<p>workshops taken: <?php echo $workshops; ?></p>
-<p>qualifications: <?php echo $qualifications; ?></p>
-<p>subject: <?php echo $subject; ?></p>
 
 <button id='accept' onclick='accept()'>accept</button>
 <button id='reject' onclick='reject()'>nope</button>
