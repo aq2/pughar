b80439ea049259761eeafdc6767f974950a561eb
angelo <angelo@fluxton.co.uk>
2019-08-05 14:13:40 +0100

    about to add editable div

---
+++ src/php/mailVetForm.php
@@ -0,0 +1,36 @@
+<?php
+
+// honeypot?
+
+require('mail/mailer.php');
+
+// where to send emails to, and where they come from
+$emailTo = 'angelo@fluxton.co.uk';
+// $emailTo = 'saghar@ayurvedicyogamassage.org.uk';
+$emailCC = 'mickey.megabyte@gmail.com';
+
+$msg = $_POST['vetbox'];
+
+echo $msg;
+
+try {
+  // Recipients
+  $mail->addAddress($emailTo);     // Add a recipient
+  $mail->setFrom($emailTo, 'from website');
+  $mail->addReplyTo('angelo@fluxton.co.uk');
+  $mail->addCC($emailCC);
+
+  // Content
+  $mail->isHTML(true);
+  $mail->Subject = 'Intensive Training Course';
+  $mail->Body    = $msg;
+  $mail->AltBody = $msg;
+
+  $mail->send();
+  print "<meta http-equiv='refresh' content='0;URL=../pages/contactThanks.html'>";
+
+} catch (Exception $e) {
+  /* echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; */
+  print "<meta http-equiv='refresh' content='0;URL=../pages/formError.html'>";
+}
+
--- src/php/processContactForm.php
+++ src/php/processContactForm.php
@@ -59,3 +59,5 @@ function safify($var) {
 }
 
 ?>
+
+
